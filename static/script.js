39a7891cfedb588b2acb63b6f10f040f811c94b3
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-05-24 17:45:03 -0400

    fix

--- static/script.js
+++ static/script.js
@@ -1,3 +1,10 @@
 let fecha = new Date().getFullYear();
 
 document.getElementById('fecha').innerHTML = `${fecha} THE BEST PRICES IN THE AREA 🌿`;
+
+const btn = document.getElementById("menu-btn");
+const links = document.getElementById("nav-links");
+
+btn.addEventListener("click", () => {
+    links.classList.toggle("active");
+});
